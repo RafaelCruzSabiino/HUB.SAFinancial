0c6edc11f209db57c091610cb4eb10067c738c8e
Rafael Sabino <rcsabino@paschoalotto.com.br>
2025-06-23 11:02:33 -0300

    feat: arquivo de configuração

---
+++ src/services/FinancialAPI/Configuration.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Hub\Financial\services\FinancialAPI;
+
+use AutoMapperPlus\AutoMapper;
+use AutoMapperPlus\AutoMapperInterface;
+use Hub\Financial\Brick\Core\config\EnvLoader;
+use AutoMapperPlus\Configuration\AutoMapperConfig;
+
+class Configuration
+{    
+    public function __construct(public EnvLoader $envLoader = new EnvLoader("FinancialAPI.env")){}
+    
+    public function ConfigureDependencies() : array
+    {
+        return [];
+    }
+
+    public function ConfigureMapper() : AutoMapperInterface
+    {
+        return AutoMapper::initialize(function (AutoMapperConfig $config) 
+        {});
+    }
+}
